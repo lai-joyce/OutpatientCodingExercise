eef99c32912508eab5c0b71e070d2cf677548ef0
Joyce Lai <lai.joyce@gmail.com>
2018-03-12 01:45:18 -0700

    API call successful for top-ranking PT based on location

--- logic.js
+++ logic.js
@@ -1,22 +1,35 @@
 'use strict';
 
 const yelp = require("yelp-fusion");
+// const request = require("request");
 
 //require Yelp Fusion API Key
 const authentication = require("./keys.js");
 
-const searchRequest = {
-	term: "Four Barrel Coffee",
-	location: "San Francisco, CA"
-};
 
-const client = yelp.client(authentication.apiKey);
+function grabPTinfo(location) {
 
-client.search(searchRequest).then(response => {
-	const firstResult = response.jsonBody.businesses[0];
-	const prettyJson = JSON.stringify(firstResult, null, 4);
-	console.log(prettyJson);
-}).catch(e => {
-	console.log(e);
-});
+	if(!location) location = "San Mateo";
+
+	const queryURL = "https://api.yelp.com/v3/businesses/?location=" + location
+	+ "&apikey=" + authentication.apiKey; 
+
+
+	const searchRequest = {
+		term: "Physical Therapists",
+		location: queryURL
+	};
+
+	const client = yelp.client(authentication.apiKey);
+
+	client.search(searchRequest).then(response => {
+		const firstResult = response.jsonBody.businesses[0];
+		const prettyJson = JSON.stringify(firstResult, null, 4);
+		console.log(prettyJson);
+	}).catch(e => {
+		console.log(e);
+	});
+}
+
+grabPTinfo("Berkeley");
 
